6a691ef0445d785f00cfc5a08983776a6e7226fd
Ridmi97 <67148053+Ridmi97@users.noreply.github.com>
2021-02-18 22:36:10 +0530

    CREATE addLeave interface

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Test from "./test";
 import theme from "./theme/Theme";
 import {MuiThemeProvider} from "@material-ui/core";
 import Layout from "./layout";
-import './App.css';
 import Index from "./views/employeeDetails/Index";
 import PersonalDetailForm from "./views/addEmployee/personalDetailForm";
 import Checkout from "./views/addEmployee/checkout";
-
+import SignIn from './components/SignIn.jsx';
+import Profile from './components/profile.jsx';
 import Menu from "./components/Menu";
 import Dashboard from "./components/Dashboard";
 
@@ -21,16 +21,25 @@ function App() {
                       <Route path="/test">
                           <Test />
                       </Route>
+                      <Route path="/dashboard">
+                          <Dashboard />
+                      </Route>
                       <Route path="/employees">
                           <Index />
                       </Route>
                       <Route path="/employee/add">
                           <Checkout />
                       </Route>
+                      <Route path="/signIn">
+                          <SignIn />
+                      </Route>
+                      <Route path="/profile">
+                          <Profile />
+                      </Route>                      
                   </Switch>
-              </Layout>
-          </BrowserRouter>
-      </MuiThemeProvider>
+              </Layout>              
+          </BrowserRouter>          
+      </MuiThemeProvider>      
   );
 }
 
