364e1328e7a6231d9f3d19a8d593a5041d25362b
Pasindu <pasindua.18@cse.mrt.ac.lk>
2021-03-03 23:08:09 +0530

    Report drop down fixed

--- src/views/reports/order-by/Dropdown.jsx
+++ src/views/reports/order-by/Dropdown.jsx
@@ -4,10 +4,12 @@ import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
+import api from "../../../api";
+import {toast} from "react-toastify";
 
 const useStyles = makeStyles((theme) => ({}));
 
-const drop = {
+const dropList = {
     dropdown: [
         {label: "Department", value: "departmentName"},
         {label: "Pay Grade", value: "payGrade"},
@@ -17,7 +19,32 @@ const drop = {
 
 const Dropdown = (props) => {
   const classes = useStyles
-
+    const [drop, setDrop] = useState(dropList);
+    useEffect(() => {
+        async function getCustomAttributes() {
+            const [res, fetchedData] = await api.user.get.customEmployeeAttributes();
+            if (res.status !== 200) {
+                toast.error(res.message);
+                return;
+            }
+            console.log(drop);
+            fetchedData.map((record) => {
+                const custom = {
+                    label: record.customColumn,
+                    value: record.customColumn,
+                }
+                dropList.dropdown.push(custom);
+            });
+            setDrop(dropList);
+            console.log(drop);
+            if (fetchedData.length === 0) {
+                toast.error("No data fetched!");
+                return;
+            }
+        }
+        getCustomAttributes();
+    }, []);
+  console.log("returnign drop", drop);
   return (
     <FormControl variant="filled" className={classes.formControl}>
       <InputLabel id="demo-simple-select-filled-label">
